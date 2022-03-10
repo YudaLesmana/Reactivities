5ee606ccd8f57bbf350b62ecb9c6100f7562bd83
yudalesmana <yuda.za92@gmail.com>
2022-03-10 16:57:19 +0700

    End of section 13

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,14 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Application;
 using Application.Activities;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
 
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,7 +14,7 @@ export default observer (function ActivityDashboard() {
         if(activityRegistry.size <= 1) loadActivities();
     }, [activityRegistry.size, loadActivities])
 
-    if (activityStore.loadingInital) return <LoadingComponent content='Loading app' />
+    if (activityStore.loadingInital) return <LoadingComponent content='Loading activities...' />
 
     return (
         <Grid>
